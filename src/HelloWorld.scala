5ac7312b51012c767209c8a4cdacb014d66aabcd
henri <henri.vallette.viallard@efrei.net>
2020-12-04 18:52:34 +0100

    Bug fixes

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -7,8 +7,8 @@ object HelloWorld {
       1,
       Location(14.0,15.5),
       List(
-        Citizen(1,randomName(), randomName()),
-        Citizen(2,randomName(), randomName())
+        Citizen(1,randomName(10), randomName(10)),
+        Citizen(2,randomName(10), randomName(10))
       ),
       List("Hello","World")
     )
@@ -17,7 +17,14 @@ object HelloWorld {
     //val x = List.fill(1000)(droneMsg);
   }
 
-  def randomName(): String = {
-    Random.alphanumeric.dropWhile(_.isDigit).toString();
+  def randomName(length: Int): String = length match{
+    case 0 => "";
+    case _ => randomName(length - 1) + randomChar();
+  }
+
+  def randomChar(): String = {
+    val min = 65;
+    val max = 90;
+    (Random.nextInt(max-min) + min).toChar.toString;
   }
 }
