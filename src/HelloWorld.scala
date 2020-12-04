b57eeb91996b8a36ce55034cf6a4171ecfb4e142
arnaudsaulou <a.saulou@gmail.com>
2020-12-04 18:22:02 +0100

    Revert "Merge branch 'henri' into develop"
    This reverts commit 7b8b6e51058b9ef9163ec42fe8d1d761ca66ca48, reversing
    changes made to 72b90ef0d8bc0b8ff7d25220d58b6f064508ec7c.

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,14 +1,6 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
-    val droneMsg = DroneMsg(
-      1,
-      Location(14.0,15.5),
-      List(
-        Citizen(1,randomName(), randomName()),
-        Citizen(2,randomName(), randomName())
-      ),
-      List("Hello","World")
     val citizenReport = CitizenReport(
       Citizen(1,"name1","firstname1"),
       Location(14.0,15.5),
@@ -21,12 +13,6 @@ object HelloWorld {
       List(citizenReport,citizenReport)
     )
 
-    println(droneMsg);
-    //val x = List.fill(1000)(droneMsg);
-  }
-
-  def randomName(): String = {
-    Random.alphanumeric.dropWhile(_.isDigit).toString();
     println(DroneReport.getDroneReportCSV(droneReport))
   }
 }
