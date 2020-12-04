c8c35f3a50ac3a1ce9d3ad00c29bb153d06597ed
arnaudsaulou <a.saulou@gmail.com>
2020-12-04 19:57:28 +0100

    Merge random name (Henri) with develop

--- src/Peaceland.scala
+++ src/Peaceland.scala
@@ -1,10 +1,11 @@
 import java.io.{File, PrintWriter}
+import scala.util.Random
 
 object Peaceland {
   def main(args: Array[String]): Unit = {
 
     val citizenReport = CitizenReport(
-      Citizen(1, "name1", "firstname1"),
+      Citizen(1, randomString(10), randomString(10)),
       Location(14.0, 15.5),
       14
     )
@@ -18,5 +19,16 @@ object Peaceland {
     val writer = new PrintWriter(new File("List.csv"))
     writer.write(DroneReport.getDroneReportCSV(droneReport))
 
+  }
+
+  def randomString(length: Int): String = length match{
+    case 0 => "";
+    case _ => randomString(length - 1) + randomChar();
+  }
+
+  def randomChar(): String = {
+    val min = 65;
+    val max = 90;
+    (Random.nextInt(max-min) + min).toChar.toString;
   }
 }
