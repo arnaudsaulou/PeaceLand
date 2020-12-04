ee5595791873cf06d1c700b98cc4fd232b096599
henri <henri.vallette.viallard@efrei.net>
2020-12-04 02:27:01 +0100

    Added new randomization names

--- src/HelloWorld.scala
+++ src/HelloWorld.scala
@@ -1,3 +1,5 @@
+import scala.util.Random
+
 object HelloWorld {
   def main(args: Array[String]): Unit = {
 
@@ -5,12 +7,23 @@ object HelloWorld {
       1,
       Location(14.0,15.5),
       List(
-        Citizen(1,"name1","firstname1"),
-        Citizen(2,"name2","firstname2")
+        Citizen(1,randomName(5).toString, randomName(5).toString),
+        Citizen(2,randomName(5).toString, randomName(5).toString)
       ),
       List("Hello","World")
     )
 
-    println(droneMsg)
+    println(droneMsg);
+    //val x = List.fill(1000)(droneMsg);
+  }
+
+  def randomName(length: Int): String = {
+    var nom: String = null;
+    if(length == 1) {
+      return new Random().nextPrintableChar().toString();
+    } else {
+      var a = new Random().nextPrintableChar().toString() + randomName(length - 1);
+      return (a);
+    }
   }
 }
