addd24e83af4f849e872673c77cb893d4fb000e9
william <3b.williamamidjogbe@gmail.com>
2021-01-06 01:51:01 +0100

    Added incrementation

--- src/Peaceland.scala
+++ src/Peaceland.scala
@@ -4,29 +4,31 @@ import scala.util.Random
 object Peaceland {
   def main(args: Array[String]): Unit = {
 
-    val citizenReport = CitizenReport(
-      Citizen(1, randomString(10), randomString(10)),
-      Random.between(1, 10),
-    )
-
     val droneReport = DroneReport(
       1,
       List("Peace", "Watcher"),
       Location(Random.between(-90.0, 90.0), Random.between(-180.0, 180.0)),
-      List.fill(1000)(citizenReport)
+      List.tabulate(1000)(citizenID => GenerateCitizenReport(citizenID))
     )
 
-    val writer = new PrintWriter(new File("List.csv"))
+    val writer = new PrintWriter(new File("ReportDrone.csv"))
     writer.write(DroneReport.getDroneReportCSV(droneReport))
 
   }
 
-  def randomString(length: Int): String = length match{
+  private def GenerateCitizenReport(citizenID : Int) = {
+    CitizenReport(
+      Citizen(citizenID+1, randomString(10), randomString(10)),
+      Random.between(1, 10),
+    )
+  }
+
+  private def randomString(length: Int): String = length match{
     case 0 => "";
     case _ => randomString(length - 1) + randomChar();
   }
 
-  def randomChar(): String = {
+  private def randomChar(): String = {
     val min = 65
     val max = 90
     (Random.nextInt(max-min) + min).toChar.toString
