7c5bf14a5dfccea2c17fade18905bf983daa54d2
henri <henri.vallette.viallard@efrei.net>
2021-01-07 23:25:28 +0100

    Added day and micIsWorking

--- src/DroneReport.scala
+++ src/DroneReport.scala
@@ -2,14 +2,15 @@ case class DroneReport(
                         idDrone: Int,
                         wordCaught: List[String],
                         location : Location,
-                        citizenReports: List[CitizenReport]
-
+                        citizenReports: List[CitizenReport],
+                        day : String,
+                        micIsWorking : Boolean
                       )
 
 object DroneReport{
 
-  def apply(idDrone: Int, wordCaught: List[String], location : Location, citizenReports: List[CitizenReport]): DroneReport
-  = new DroneReport(idDrone, wordCaught, location, citizenReports)
+  def apply(idDrone: Int, wordCaught: List[String], location : Location, citizenReports: List[CitizenReport], day : String, micIsWorking : Boolean): DroneReport
+  = new DroneReport(idDrone, wordCaught, location, citizenReports, day, micIsWorking)
 
   def getDroneReportCSV(droneReport: DroneReport): String = {
     droneReport.idDrone +
